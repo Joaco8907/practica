a02f1ca0c24111115e425ad74bc25fe412de011d
Joaco8907 <Afscaiser@gmail.com>
2023-04-03 16:41:49 -0300

    Tercer ejercicio POO resuelto

--- Ejercicios_practica/src/Guia_7_POO_Practica/Ejercicio_tres_POO_practica.java
+++ Ejercicios_practica/src/Guia_7_POO_Practica/Ejercicio_tres_POO_practica.java
@@ -1,20 +1,43 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package Guia_7_POO_Practica;
 
-/**
- *
- * @author Afsca
- */
-public class Ejercicio_tres_POO_practica {
+import Atributos_operaciones.Operaciones;
 
-    /**
-     * @param args the command line arguments
-     */
+
+public class Ejercicio_tres_POO_practica {
+    /*
+    Crear una clase llamada Operacion que tenga como atributos privados numero1 y numero2. 
+    A continuación, se deben crear los siguientes métodos:
+        Método constructor con todos los atributos pasados por parámetro.
+        Método constructor sin los atributos pasados por parámetro.
+        Métodos get y set.
+        Método para crearOperacion(): que le pide al usuario los dos números y los guarda en los atributos del objeto.
+        Método sumar(): calcular la suma de los números y devolver el resultado al main.
+        Método restar(): calcular la resta de los números y devolver el resultado al main
+        Método multiplicar(): primero valida que no se haga una multiplicación por cero, si fuera a multiplicar por cero, el método devuelve 0 y se le informa al usuario el error. Si no, se hace la multiplicación y se devuelve el resultado al main
+        Método dividir(): primero valida que no se haga una división por cero, si fuera a pasar una división por cero, el método devuelve 0 y se le informa al usuario el error se le informa al usuario. Si no, se hace la división y se devuelve el resultado al main.
+
+    */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        Operaciones operaciones = new Operaciones (1,2);
+        System.out.println("Valor actual de Numero1= "+ operaciones.getNumero1());
+        System.out.println("Valor actual de Numero2= "+ operaciones.getNumero2());
+        
+       operaciones.crearOperaciones();
+        System.out.println("El resultado de la suma de los dos numeros modificados es: " + operaciones.sumar());
+        System.out.println("El resultado de la resta de los numeros modificados es: "+ operaciones.restar());
+        
+        if (operaciones.multiplicar()!=0){
+            System.out.println("El resultado de la multiplicacion de los numeros modificados es: "+ operaciones.multiplicar());
+        }else{
+            System.out.println("Error");
+        }
+        
+        if (operaciones.dividir()!=0){
+            System.out.println("El resultado de la division de los numeros modificados es: "+ operaciones.dividir());
+        }
+        
     }
     
 }
